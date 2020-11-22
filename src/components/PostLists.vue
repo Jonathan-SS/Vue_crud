9e410784032e5b7cf1f1c8381f7d94185d92c691
Jonathan-SS <jonathanstensgaard@gmail.com>
2020-11-22 15:19:14 +0100

    search funktion

--- src/components/PostLists.vue
+++ src/components/PostLists.vue
@@ -1,18 +1,21 @@
 <template>
-  <div class="grid-container">
-    <article v-for="post in posts" :key="post.id">
-      <div>
-        <img :src="post.img" alt="" />
-        <p>{{ post.description }}</p>
-      </div>
+  <div>
+    <input type="text" v-on:keyup="searchFunktion" v-model="searchValue" />
+    <div class="grid-container">
+      <article v-for="post in posts" :key="post.id">
+        <div>
+          <img :src="post.img" alt="" />
+          <p>{{ post.description }}</p>
+        </div>
 
-      <div>
-        <router-link :to="{ name: 'Update', params: { post: post } }">
-          <button>Update</button>
-        </router-link>
-        <button type="button" v-on:click="sletPost(post.id)">Delete</button>
-      </div>
-    </article>
+        <div>
+          <router-link :to="{ name: 'Update', params: { post: post } }">
+            <button>Update</button>
+          </router-link>
+          <button type="button" v-on:click="sletPost(post.id)">Delete</button>
+        </div>
+      </article>
+    </div>
   </div>
 </template>
 
@@ -21,6 +24,7 @@ import { postRef } from "../firebase-db.js";
 export default {
   data() {
     return {
+      searchValue:'',
       posts: [],
     };
   },
@@ -28,6 +32,25 @@ export default {
 sletPost(id){
 
   postRef.doc(id).delete()
+},
+searchFunktion(){
+console.log(this.searchValue)
+let searchValueLower=this.searchValue.toLowerCase()
+console.log(searchValueLower)
+let filteredPosts=[]
+ 
+for (const post of this.posts) {
+ 
+if(post.description.toLowerCase().includes(this.searchValue.toLowerCase())){
+  filteredPosts.push(post)
+
+console.log(filteredPosts)
+this.posts=filteredPosts
+  }
+  
+
+  }
+
 }
  },
   firestore: {
